441fa82635b3d26dbc1fd97ee4a359c0fddec88c
Nathan McKenzie <nathantaylormckenzie@hotmail.com>
2021-03-12 16:26:05 -0800

    Adding Chat component

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.css";
+import Chat from './Chat'
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "shards-ui/dist/css/shards.min.css"
 
-const App = () => <div>Hi there, I'm React from Webpack 5.</div>;
+const App = () => <div><Chat /></div>;
 
 ReactDOM.render(<App />, document.getElementById("app"));
